a59f38a2a46eb2f96939c3306d29f82a7b0627ec
Muhammed Imthiyas Alam <developerimthiyas@gmail.com>
2025-01-10 22:46:56 +0530

    teversed array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,23 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int numbers[]  = {1,2,3,4,5,6,7,8,9,0};
+        int left = 0;
+        int right = numbers.length-1;
+
+        while (left<right) {
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+
+            left++;
+            right--;
+
+        }
+
+
+        System.out.println("Reversed Array is:");
+        for(int num:numbers){
+            System.out.print(num+" ");
+        }
+    }
+}
