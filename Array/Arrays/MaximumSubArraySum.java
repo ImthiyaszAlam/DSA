6bba2bc5ca48e79d421beb27560be36ad7d4ce0f
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-01-31 23:03:29 +0530

    findMaximumSubArraySum

--- Array/Arrays/MaximumSubArraySum.java
+++ Array/Arrays/MaximumSubArraySum.java
@@ -1,6 +1,23 @@
 public class MaximumSubArraySum {
+
+    public static int findMaxSubArraySum(int arr[]) {
+        int currentSum = 0;
+        int maxSumSoFar = Integer.MIN_VALUE;
+
+        for (int currentElement : arr) {
+            currentSum += currentElement;
+            maxSumSoFar = Math.max(maxSumSoFar, currentSum);
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSumSoFar;
+    }
+
     public static void main(String[] args) {
-        
-        int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4}; 
+
+        int arr[] ={-2, 1, -3, 4, -1, 2, 1, -5, 4}; ;
+        System.out.println(findMaxSubArraySum(arr));
     }
 }
