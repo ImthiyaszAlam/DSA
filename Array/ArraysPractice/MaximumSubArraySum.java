7eafed2bef5649664c23c89f665ce76c64e66496
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-01-31 23:15:27 +0530

    practiced maxSubArrSum

---
+++ Array/ArraysPractice/MaximumSubArraySum.java
@@ -0,0 +1,24 @@
+public class MaximumSubArraySum {
+
+    public static int findMaxSubArraySum(int arr[]){
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int num:arr){
+            currentSum+=num;
+            maxSum = Math.max(maxSum, currentSum);
+
+            if (currentSum<0) {
+                currentSum=0;
+            }
+
+         
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,-3,4,-5,5,-7};
+        System.out.println(findMaxSubArraySum(arr));
+    }
+}
