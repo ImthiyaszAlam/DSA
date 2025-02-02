5a7f214e0e1f813223b28bc36604678976ec445b
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-02-03 00:20:45 +0530

    firstAndLastPositionBruteForce

--- Array/Arrays/FirstLastPosition.java
+++ Array/Arrays/FirstLastPosition.java
@@ -5,6 +5,24 @@ public class FirstLastPosition {
     }   
      
     public static void main(String[] args) {
-        
+        int arr[] = {1,2,3,4,4,5};
+        int target = 4;
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<end) {
+            if (arr[start] == target) {
+                System.out.print("first index : "+start);
+            }
+
+            System.out.println();
+
+            if (arr[end] == target) {
+                System.out.print("last index: "+end);
+            }
+
+            start++;
+            end--;
+        }
     }
 }
