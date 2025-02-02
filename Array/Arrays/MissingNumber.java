a5eaf63f0b4f803b7dbe058cd192ce6cc6ef247a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-02-03 00:05:38 +0530

    find missing number from given array (sorted)

--- Array/Arrays/MissingNumber.java
+++ Array/Arrays/MissingNumber.java
@@ -1,9 +1,19 @@
 public class MissingNumber {
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7,8,9};
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+
+    public static int findMissingNumber(int nums[]){
+        int actualSum = 0;
+        int totalNumber = nums.length;
+        int totalSum = totalNumber *(totalNumber+1) /2;
+        for(int num:nums){
+            actualSum+=num;
         }
+
+        return totalSum-actualSum;
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,3,2,1,0};
+        System.out.println(findMissingNumber(arr));
+
     }
 }
