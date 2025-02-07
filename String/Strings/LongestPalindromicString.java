eecb41b1e214d174d7ad4ef3263c1bf45b05c6b3
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-02-07 23:30:45 +0530

    declared methods for getting result

--- String/Strings/LongestPalindromicString.java
+++ String/Strings/LongestPalindromicString.java
@@ -1,5 +1,20 @@
 public class LongestPalindromicString {
-    public static void main(String[] args) {
+
+
+    public static String findLongestPalString(String str){
+        if (str == null || str.length()<1) {
+            return "";
+        }
+        int start = 0;
+        int end = 0;
+
+    }
+
+    private static int expandAroundCenter(String s, int left ,int right){
         
+    }
+    public static void main(String[] args) {
+        String str = "babad";
+        findLongestPalString(str);
     }
 }
