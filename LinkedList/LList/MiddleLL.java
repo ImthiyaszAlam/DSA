e7ea33073502c19375e5f1f9a101c4216f292548
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-02-08 00:30:46 +0530

    started with LinkedList

---
+++ LinkedList/LList/MiddleLL.java
@@ -0,0 +1,19 @@
+
+
+public class MiddleLL {
+Node
+    
+        public static void findMiddle(){
+            int count = 0;
+            Node temp = hNode;
+
+        while (temp!=null) {
+            count++;
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
