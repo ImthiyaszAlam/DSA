1de5f984463d81d266227d8f92bf49d1a26934ed
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-02-08 23:24:52 +0530

    LinkedList insertAtEnd()

--- LinkedList/LList/SingleLL.java
+++ LinkedList/LList/SingleLL.java
@@ -2,24 +2,42 @@ public class SingleLL {
 
     Node hNode;
 
-    void insertAtBeginning(int data){
+    void insertAtBeginning(int data) {
         Node newNode = new Node(data);
         newNode.next = hNode;
         hNode = newNode;
     }
 
-    void printLList(){
+    void insertAtEnd(int data) {
+        Node node = new Node(data);
+        if (hNode == null) {
+            node = hNode;
+            return;
+        }
+
         Node temp = hNode;
-        while (temp!=null) {
-            System.out.print(temp.data+"->");
-            temp=temp.next;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = hNode;
+    }
+
+    void printLList() {
+        Node temp = hNode;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
         }
         System.out.println("NULL");
     }
+
     public static void main(String[] args) {
         SingleLL list = new SingleLL();
         list.insertAtBeginning(1);
         list.insertAtBeginning(2);
+
+        list.insertAtEnd(4);
+        list.insertAtEnd(5);
 
         list.printLList();
     }
@@ -29,7 +47,7 @@ class Node {
     int data;
     Node next;
 
-    Node(int d){
+    Node(int d) {
         this.data = d;
         this.next = null;
     }
