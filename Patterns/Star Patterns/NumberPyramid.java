d2c71333888e19f945754364f9d049a41dfe8b28
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-12 17:46:50 +0530

    number pyramid

--- Patterns/Star Patterns/NumberPyramid.java
+++ Patterns/Star Patterns/NumberPyramid.java
@@ -1,5 +1,23 @@
 public class NumberPyramid {
     public static void main(String[] args) {
-        
+
+        int n = 5;
+        int num = 100;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" "+num);
+                num++;
+
+                if (num>=1000) {
+                    num=1;
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
