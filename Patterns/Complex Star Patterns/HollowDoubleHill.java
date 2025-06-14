15cacd35af6ec0fde6bc543e153f25588bc37c87
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-15 00:49:29 +0530

    HollowDouble

--- Patterns/Complex Star Patterns/HollowDoubleHill.java
+++ Patterns/Complex Star Patterns/HollowDoubleHill.java
@@ -47,6 +47,51 @@ public class HollowDoubleHill {
 
             // next Line
             System.out.println();
+
+        }
+
+        // lOWER HALF
+
+        for (int i = 0; i < n; i++) {
+
+            // space
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            // star
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                int firstIndexInRow = 0;
+                int lastIndexInRow = n-i-2;
+
+                if (j == lastIndexInRow || j == firstIndexInRow) {
+                    System.out.print("*"+" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            // space between pyramids
+
+              // Spaces between inverted pyramids
+              for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+      
+            for (int j = 0; j < n - i - 1; j++) {
+                if (j == 0 || j == n - i - 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+
         }
 
     }
