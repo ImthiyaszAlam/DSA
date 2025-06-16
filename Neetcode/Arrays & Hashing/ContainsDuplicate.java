ca48a9c9f27328e37e3133ee69aeaffb361bc05d
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-16 16:00:06 +0530

    containsDuplicate

---
+++ Neetcode/Arrays & Hashing/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int nums[]) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = { 1, 2, 3, 1 };
+        int nums1[] = { 1, 2, 3, 4 };
+
+        System.out.println(containsDuplicate(nums));
+        ;
+        System.out.println();
+        System.out.println(containsDuplicate(nums1));
+        ;
+
+    }
+}
