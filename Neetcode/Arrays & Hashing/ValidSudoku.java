ead402c8ebedc8979362d6891edc02460d4acc9a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-23 12:11:08 +0530

    Leetcode-36-Valid-Sudoku

--- Neetcode/Arrays & Hashing/ValidSudoku.java
+++ Neetcode/Arrays & Hashing/ValidSudoku.java
@@ -6,7 +6,7 @@ public class ValidSudoku {
 
         int n = sudokuBoard.length;
 
-        HashSet<Character> seenKey = new HashSet<>();
+        HashSet<String> seenKey = new HashSet<>();
 
         // Row
         for (int i = 0; i < n; i++) {
@@ -17,12 +17,17 @@ public class ValidSudoku {
 
                 if (currentElement != '.') {
                     String rowKey = "row_" + i + "_" + currentElement;
-                    System.out.print(rowKey+",");
+
                     String colKey = "col_" + j + "_" + currentElement;
-                    System.out.print(colKey+",");
+
+                    String boxKey = "box_" + (i / 3) + (j / 3) + "_" + currentElement;
+
+                    if (!seenKey.add(rowKey) || !seenKey.add(colKey) || !seenKey.add(boxKey)) {
+                        return false;
+                    }
+
                 }
             }
-            System.out.println();
 
         }
         return true;
@@ -34,13 +39,13 @@ public class ValidSudoku {
                 { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
                 { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
                 { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                { '4', '.', '.', '8', '.', '3', '8', '.', '1' },
                 { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
                 { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
                 { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
                 { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
         };
 
-        isValidSudoku(board);
+        System.out.println(isValidSudoku(board));;
     }
 }
