0ab77ef839b8aab65cac3243e87efed82c038a1a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-24 16:25:25 +0530

    encode with stringBuilder

--- Neetcode/Arrays & Hashing/EncodeDecode.java
+++ Neetcode/Arrays & Hashing/EncodeDecode.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class EncodeDecode {
 
-    public static void encode(String strs[]) {
+    public static String encode(String strs[]) {
+
+        StringBuilder encodedList = new StringBuilder();
 
         for (int i = 0; i < strs.length; i++) {
             String currentWord = strs[i];
             int currentWordLength = currentWord.length();
             String currentWordToEncode = currentWord + currentWordLength + "#";
+            encodedList.append(currentWordLength).append("#").append(currentWord);
         }
+
+        return encodedList.toString();
     }
 
     public static void main(String[] args) {
+
+        String strs[] = { "apple", "banana", "grapes", "jamun" };
+        System.out.println(encode(strs));
 
     }
 }
