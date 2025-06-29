410b94c665c1ae7eb03c2747f4c1ab749c4144e1
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-06-29 15:20:19 +0530

    ConcentSquarePattern

---
+++ Patterns/Complex Star Patterns/ConcentSquarePattern.java
@@ -0,0 +1,20 @@
+public class ConcentSquarePattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                int layer = Math.min(Math.min(i, j), Math.min(n - i - 1, n - j - 1));
+
+                if (layer % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("#");
+                }
+            }
+        }
+      //  System.out.println();
+    }
+}
