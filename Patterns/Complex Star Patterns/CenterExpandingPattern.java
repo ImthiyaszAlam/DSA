d6f6868a83ea7c1f91035b922f260fd8b7e66c55
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-06 19:55:33 +0530

    center expanding

---
+++ Patterns/Complex Star Patterns/CenterExpandingPattern.java
@@ -0,0 +1,30 @@
+public class CenterExpandingPattern {
+
+    public static void printCenterExpandingPattern(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            int j;
+            for (j = 0; j <= n - i; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.print(j);
+
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        printCenterExpandingPattern(n);
+
+    }
+}
