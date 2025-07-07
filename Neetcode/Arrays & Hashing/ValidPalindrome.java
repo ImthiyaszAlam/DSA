d54d753589087f5e71f69d58568362f15fc18ac0
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-07 15:59:22 +0530

    valid palindrome-code

--- Neetcode/Arrays & Hashing/ValidPalindrome.java
+++ Neetcode/Arrays & Hashing/ValidPalindrome.java
@@ -1,3 +1,35 @@
 public class ValidPalindrome {
-    
+
+    public static boolean isPalindrome(String s) {
+        int firstIndex = 0;
+        int lastIndex = s.length() - 1;
+
+        while (firstIndex < lastIndex) {
+
+            // Skip non-alphanumeric from start
+            while (firstIndex < lastIndex && !Character.isLetterOrDigit(s.charAt(firstIndex))) {
+                firstIndex++;
+            }
+
+            // Skip non-alphanumeric from end
+            while (firstIndex < lastIndex && !Character.isLetterOrDigit(s.charAt(lastIndex))) {
+                lastIndex--;
+            }
+
+            // Compare characters in lowercase
+            if (Character.toLowerCase(s.charAt(firstIndex)) != Character.toLowerCase(s.charAt(lastIndex))) {
+                return false;
+            }
+
+            firstIndex++;
+            lastIndex--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str)); // Output: true
+    }
 }
