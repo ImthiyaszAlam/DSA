1a65dc575ede57948b833588db0bf866b215530c
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-07-20 18:30:47 +0530

    sliding window practice

--- Leetcode/Sliding Window/MinimumWindowSubstring.java
+++ Leetcode/Sliding Window/MinimumWindowSubstring.java
@@ -14,8 +14,20 @@ public class MinimumWindowSubstring {
             tCount.put(currentElement, tCount.getOrDefault(currentElement, tCount.getOrDefault(tCount, 0) + 1));
         }
 
+        // Sliding Window
 
-        //Sliding Window
+        int minLeft = 0;
+        int minLen = 0;
+        Map<Character, Integer> sCount = new HashMap<>();
+
+        for (int right = 0; right < s.length(); right++) {
+
+            if (sCount.containsKey(s.charAt(right), sCount.getOrDefault(s.charAt(right), 0) + 1)) {
+
+            }
+
+            sCount.put(s.charAt(right), sCount.getOrDefault(s.charAt(right), 0) + 1);
+        }
     }
 
     public static void main(String[] args) {
