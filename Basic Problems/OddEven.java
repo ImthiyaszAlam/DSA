b8a9826abbdf9f5611a11795def2b29ddaa1b0a7
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-09-25 00:39:13 +0530

    odd-even

---
+++ Basic Problems/OddEven.java
@@ -0,0 +1,13 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        if (a % b != 0) {
+            System.out.println("Odd");
+        } else {
+            System.out.println("Even");
+        }
+
+    }
+}
