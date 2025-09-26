d693fcd5308ca63dffc4e51301c71c0bf5f9f4fe
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-09-26 21:33:31 +0530

    Natural sum

---
+++ Basic Problems/NaturalSum.java
@@ -0,0 +1,12 @@
+public class NaturalSum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i=0;i<10;i++){
+            int currentNum = i;
+            sum+=currentNum;
+        }
+
+        System.out.println(sum);
+    }
+}
