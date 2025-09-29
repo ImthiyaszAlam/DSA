aeca0628aca58fd02fde37688396debfef3ad0d8
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-09-29 22:45:50 +0530

    panagram

--- GFG/String/CheckPanagram.java
+++ GFG/String/CheckPanagram.java
@@ -1,8 +1,26 @@
 public class CheckPanagram {
-    public static void main(String[] args) {
 
+    public static boolean idPanagram(String str) {
         for (char currentChar = 'a'; currentChar <= 'z'; currentChar++) {
 
+            boolean found = false;
+            for (int i = 0; i < str.length(); i++) {
+                if (currentChar == Character.toLowerCase(str.charAt(i))) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false) {
+                return false;
+            }
         }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "The quick brown fox jumps over the lazy";
+        System.out.println(idPanagram(str));
+
     }
 }
