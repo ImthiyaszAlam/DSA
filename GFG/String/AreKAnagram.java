d8a80cbea4af49abdb631958c65d123b22ea4d07
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-03 21:24:28 +0530

    areKanagram

---
+++ GFG/String/AreKAnagram.java
@@ -0,0 +1,10 @@
+public class AreKAnagram {
+
+
+    public static boolean isKAnagram(String s1,String s2){
+        return true;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
