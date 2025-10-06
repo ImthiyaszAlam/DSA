333f5473f66779594b4d0bc002b6e979e2445308
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-06 21:46:27 +0530

    searchString

--- GFG/String Basic/findChar.java
+++ GFG/String Basic/findChar.java
@@ -1,13 +1,18 @@
 public class findChar {
-    public static void main(String[] args) {
-        String str = "malam";
-        char k = 'a';
 
-        for(int i=0;i<str.length();i++){
-            if (str.charAt(i)==k) {
-                System.out.println(i);
-                return;
+    public static int findChar(String str, char ch) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                return i;
             }
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String str = "malam";
+        char ch = 'a';
+        System.out.println(findChar(str, ch));
+
     }
 }
