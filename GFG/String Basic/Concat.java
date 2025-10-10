2e2b1e543d129194181c1b729710ab6274accc15
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-10 20:47:02 +0530

    string concatenation

--- GFG/String Basic/Concat.java
+++ GFG/String Basic/Concat.java
@@ -1,14 +1,29 @@
 public class Concat {
 
+    public static String concatSb(String s1, String s2) {
+        StringBuilder result = new StringBuilder();
 
-    public static String concat(String s1,String s2){
+        for (char ch : s1.toCharArray()) {
+            result.append(ch);
+        }
+
+        for (char ch : s2.toCharArray()) {
+            result.append(ch);
+        }
+
+        return result.toString();
+
+    }
+
+    public static String concat(String s1, String s2) {
         String result;
-        result = s1+s2;
+        result = s1 + s2;
         return result;
     }
+
     public static void main(String[] args) {
         String s1 = "Al";
         String s2 = "am";
-        System.out.println(concat(s1, s2));
+        System.out.println(concatSb(s1, s2));
     }
 }
