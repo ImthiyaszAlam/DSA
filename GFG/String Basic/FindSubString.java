9ec67405ca3ef0c54113444f0b22dcf7e516b320
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-12 21:29:52 +0530

    substring

--- GFG/String Basic/FindSubString.java
+++ GFG/String Basic/FindSubString.java
@@ -6,16 +6,21 @@ public class FindSubString {
     public static List<String> findSubstring(String str) {
 
         List<String> result = new ArrayList<>();
+        int count = 0;
 
         for (int i = 0; i < str.length() - 1; i++) {
-            for (int j = i; j < str.length() - 1; j++) {
-                result.add(i, j + 1);
+            for (int j = i; j < str.length(); j++) {
+                result.add(str.substring(i, j + 1));
+                count++;
             }
         }
+        System.out.println(count);
+        
         return result;
     }
 
     public static void main(String[] args) {
-
+        String str = "alam";
+        System.out.println(findSubstring(str));
     }
 }
