496af8c20aab1078a36a71a5d1dea87a2ec7533a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-14 22:49:30 +0530

    addToBinary

--- GFG/String Medium/AddTwoBinary.java
+++ GFG/String Medium/AddTwoBinary.java
@@ -5,6 +5,20 @@ public class AddTwoBinary {
         return (firstOne == -1 )? "0" : str.substring(firstOne);
     }
 
+
+    public static String addTwoBinary(String s1,String s2){
+        s1 = trimLeadingZaros(s2);
+        s2=trimLeadingZaros(s2);
+
+
+        if (s1.length()<s2.length()) {
+            return addTwoBinary(s1, s2);
+        }
+
+        int j = s2.length()-1;
+        int carry = 0;
+    }
+
     public static void main(String[] args) {
 
     }
