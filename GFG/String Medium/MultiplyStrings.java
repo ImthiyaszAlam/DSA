7bfc713d81092e422b5fd942c15fca59c14b2134
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-16 21:44:19 +0530

    multiply Strings-Medium

--- GFG/String Medium/MultiplyStrings.java
+++ GFG/String Medium/MultiplyStrings.java
@@ -1,48 +1,60 @@
 public class MultiplyStrings {
 
     public static String multiplyStrings(String s1, String s2) {
+        int n1 = s1.length(), n2 = s2.length();
 
-        int n1 = s1.length();
-        int n2 = s2.length();
-
-        if (n1 == 0 || n2 == 0) {
+        if (n1 == 0 || n2 == 0)
             return "0";
-        }
 
-        int nn = 1;
-        int mm = 1;
-
+        int sign = 1;
         if (s1.charAt(0) == '-') {
-            nn = -1;
+            sign *= -1;
+            s1 = s1.substring(1);
+            n1--;
         }
-
         if (s2.charAt(0) == '-') {
-            mm = -1;
+            sign *= -1;
+            s2 = s2.substring(1);
+            n2--;
         }
 
-        int isNegative = nn * mm;
         int[] result = new int[n1 + n2];
 
-        int i1 = 0;
-        int i2 = 0;
-
+        // multiply each digit
         for (int i = n1 - 1; i >= 0; i--) {
-            if (s1.charAt(i) == '-')
-                continue;
             int carry = 0;
             int n1Digit = s1.charAt(i) - '0';
-            i2 = 0;
 
             for (int j = n2 - 1; j >= 0; j--) {
-                if (s2.charAt(j) == '-')
-                    continue;
                 int n2Digit = s2.charAt(j) - '0';
-
+                int sum = n1Digit * n2Digit + result[i + j + 1] + carry;
+                carry = sum / 10;
+                result[i + j + 1] = sum % 10;
             }
+            result[i] += carry;
         }
+
+        // convert to string
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < result.length && result[i] == 0)
+            i++;
+
+        if (i == result.length)
+            return "0";
+
+        for (; i < result.length; i++)
+            sb.append(result[i]);
+
+        if (sign == -1)
+            sb.insert(0, '-');
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-
+        String s1 = "-10";
+        String s2 = "10";
+        System.out.println(multiplyStrings(s1, s2));  
     }
 }
