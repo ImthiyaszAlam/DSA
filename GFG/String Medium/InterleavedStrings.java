a6140971e898ac194153f9c310561a72539ca232
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-21 13:21:25 +0530

    isInterLeaved solved by recursively checking their length and check for indexes using three string s and i,j and k vars to track their length in case it traverse all and reach all these i,j and k with length of s1,s2 and s3 then true

--- GFG/String Medium/InterleavedStrings.java
+++ GFG/String Medium/InterleavedStrings.java
@@ -1,12 +1,32 @@
 public class InterleavedStrings {
 
-    public static boolean isInterLeaved(String s1,String s2,String s3,int i,int j){
+    public static boolean isIL(String s1, String s2, String s3, int i, int j) {
 
-        
-        int k = i+j;
-        
+        int k = i + j;
+
+        if (i == s1.length() && j == s2.length() && k == s3.length()) {
+            return true;
+        }
+
+        boolean a = (i < s1.length()) && s3.charAt(k) == s1.charAt(i) && isIL(s1, s2, s3, i + 1, j);
+        boolean b = (j < s2.length()) && s3.charAt(k) == s2.charAt(j) && isIL(s1, s2, s3, i, j + 1);
+
+        return a || b;
+
     }
+
+    public static boolean isInterLeaved(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        return isIL(s1, s2, s3, 0, 0);
+    }
+
     public static void main(String[] args) {
-        
+        String s1 = "AAB";
+        String s2 = "AAC";
+        String s3 = "AAAABC";
+        System.out.println(isInterLeaved(s1, s2, s3));
     }
 }
