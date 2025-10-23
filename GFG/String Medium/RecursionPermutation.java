1db2cff2b69ff9f9ea1ea5ad7351dfa1f8ae83c0
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-23 22:06:39 +0530

    string permutation-swap

--- GFG/String Medium/RecursionPermutation.java
+++ GFG/String Medium/RecursionPermutation.java
@@ -3,10 +3,20 @@ import java.util.List;
 public class RecursionPermutation {
 
     public static void recurPerm(int index, StringBuilder str, List<String> ans) {
-        if (index == str.length())
+        if (index == str.length()) {
             ans.add(str.toString());
-        return;
+            return;
+        }
 
+        for (int i = index; i < str.length(); i++) {
+            swap(str,index,i);
+        }
+    }
+
+    public static void swap(StringBuilder str,int i,int j){
+        char temp = str.charAt(i);
+        str.setCharAt(i, str.charAt(j));
+        str.setCharAt(j, temp);
     }
 
     public static void main(String[] args) {
