c635f5b62580f08ae28835ebac32fa3da9898653
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-24 21:19:06 +0530

    longest Substring-palindrome Using two loops iterating each by making substring using a variable maxLen to track

--- GFG/String Medium/LongestPalindromicString.java
+++ GFG/String Medium/LongestPalindromicString.java
@@ -11,7 +11,30 @@ public class LongestPalindromicString {
 
         return true;
     }
+
+
+
+    static String longestPalindrome(String str){
+
+        int n= str.length();
+        int maxLen = 1;
+        int start = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if (checkPal(str, i, j) && (j-i+1)>maxLen) {
+                    start = i;
+                    maxLen = j-i+1;
+                }
+            }
+        }
+
+        return str.substring(start, start+maxLen);
+    }
+
+
     public static void main(String[] args) {
-        
+        String str = "forgeeksskeegfor";
+        System.out.println(longestPalindrome(str));
     }
 }
