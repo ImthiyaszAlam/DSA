9cf4e3d0830998c1b5df3def380f3b427b4a4a0e
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-26 12:44:11 +0530

    binary strings w/o consecutive 1s

---
+++ GFG/String Medium/BinaryStringsWO1.java
@@ -0,0 +1,11 @@
+public class BinaryStringsWO1 {
+
+    static int countRecur(int i,int n){
+        if (i>=n) {
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
