4250c6a43f5f7ca6d5f7d2183766823b5edac44e
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-29 15:54:08 +0530

    Lexicographic

--- GFG/String Medium/LexicoGraphicString.java
+++ GFG/String Medium/LexicoGraphicString.java
@@ -1,4 +1,20 @@
 public class LexicoGraphicString {
+
+    static String nmextString(String str){
+        int i = str.length()-1;
+        while (i>=0 && str.charAt(i)=='z') {
+            i--;
+        }
+
+        if (i==-1) {
+            return str+'a';
+        }
+
+        char [] charArray = str.toCharArray();
+        charArray[i]++;
+
+        return new String(charArray,0,i+1);
+    }
     public static void main(String[] args) {
         
     }
