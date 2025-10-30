b7b69ff5872837ab0a2e401d5d6c0bba56737d4a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-10-30 23:20:54 +0530

    strCheck

--- GFG/String Medium/SplitString.java
+++ GFG/String Medium/SplitString.java
@@ -1,5 +1,14 @@
 public class SplitString {
+
+    public static boolean strCheck(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
