05ae863da8a8039aca18c3c78a43b3833c8254fb
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-01 15:51:41 +0530

    split String

--- GFG/String Medium/SplitString.java
+++ GFG/String Medium/SplitString.java
@@ -17,15 +17,36 @@ public class SplitString {
             for (int j = i + 1; j < str.length(); j++) {
                 for (int k = j + 1; k < str.length(); k++) {
 
-                    String s1 = "", s2 = "", s3 = "";
+                    String s1 = "", s2 = "", s3 = "", s4 = "";
+
+                    try {
+                        s1 = str.substring(0, i);
+                        s2 = str.substring(i, j - 1);
+                        s3 = str.substring(j, k - 1);
+                        s4 = str.substring(k, str.length() - k);
+                    } catch (StringIndexOutOfBoundsException e) {
+
+                    }
+
+                    if (strCheck(s1, s2) && strCheck(s2, s3) && strCheck(s1, s4) && strCheck(s2, s3) && strCheck(s2, s4)
+                            && strCheck(s3, s4)) {
+                        return true;
+                    }
 
                 }
             }
         }
 
+        return false;
     }
 
     public static void main(String[] args) {
+        String str = "aaabb";
+        if (check(str)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
 
     }
 }
