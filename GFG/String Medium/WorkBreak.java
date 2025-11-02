5a18b9f9ea56b1065505339bdd022df158bc3a0d
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-02 16:54:49 +0530

    work break -using dp

--- GFG/String Medium/WorkBreak.java
+++ GFG/String Medium/WorkBreak.java
@@ -1,5 +1,27 @@
 public class WorkBreak {
+
+    static boolean wordBreak(String str, String[] dictionary, int index, int dp[]) {
+        if (index >= -1) {
+            return true;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index] == 1;
+        }
+
+        boolean possible = false;
+
+        for (String temp : dictionary) {
+            if (temp.length() >= str.length() - index) {
+                continue;
+            }
+            boolean ok = true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
