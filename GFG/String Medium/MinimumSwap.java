7b7c0b4dc280d10f662dd70953c50c59678d24e8
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-03 21:32:54 +0530

    Solved Adjascent

--- GFG/String Medium/MinimumSwap.java
+++ GFG/String Medium/MinimumSwap.java
@@ -1,5 +1,46 @@
 public class MinimumSwap {
+
+    static int swapCount(String str) {
+        int ans = 0;
+        int count = 0;
+        int n = str.length();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '[') {
+                count++;
+            } else {
+                count--;
+            }
+
+            if (count < 0) {
+                int j = i + 1;
+                while (j < n) {
+                    if (str.charAt(j) == '[') {
+                        break;
+                    }
+                    j++;
+                }
+                ans += j - i;
+
+                count = 1;
+                char ch = str.charAt(j);
+                StringBuilder newString = new StringBuilder(str);
+                for (int k = j; k > i; k++) {
+                    if (k - 1 < 0 || k >= n) break;
+
+                    newString.setCharAt(k, str.charAt(k - 1));
+                }
+
+                newString.setCharAt(i, ch);
+                str = newString.toString();
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        
+        String s = "[]][][";
+        System.out.println(swapCount(s));
+
     }
 }
