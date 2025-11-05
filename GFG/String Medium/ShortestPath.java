b05d0e9f0650b7f9bbab1b56609c27acf53ab6d1
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-05 23:05:56 +0530

    shortest

--- GFG/String Medium/ShortestPath.java
+++ GFG/String Medium/ShortestPath.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ShortestPath {
 
-    
+    static ArrayList<String> printPath(String str){
+        ArrayList<String>result = new ArrayList<>();
+        HashMap<Character,int[]> position = new HashMap<>();
+        char ch = 'A';
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5 && ch<='Y';j++;ch++){
+
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         
     }
