e40b3035db3f921c8bf24151f2bef4a5117ce6a3
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-07 22:54:24 +0530

    multiplyString

---
+++ GFG/String Hard/MultiplyString.java
@@ -0,0 +1,20 @@
+public class MultiplyString {
+
+    static String multiplyStrings(String s1,String s2){
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if (n1==0 || n2==0) {
+            return "0";
+        }
+
+        int nn= 1,mm=1;
+        if (s1.charAt(0)=='-') {
+            nn=-1;
+        }
+
+    } 
+    public static void main(String[] args) {
+        
+    }
+}
