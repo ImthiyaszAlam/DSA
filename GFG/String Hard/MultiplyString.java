6386cfb639d15b76b440397002ea526364aea06c
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-07 23:00:13 +0530

    Multiply Strings half

--- GFG/String Hard/MultiplyString.java
+++ GFG/String Hard/MultiplyString.java
@@ -13,6 +13,38 @@ public class MultiplyString {
             nn=-1;
         }
 
+        if (s2.charAt(0)=='-') {
+            mm=-1;
+        }
+
+        int isNegative = nn*mm;
+        int result[] = new int[n1+n2];
+
+        int i1=0;
+        int i2 = 0;
+
+
+
+        for(int i=n1-1;i>=0;i--){
+            if (s1.charAt(i)=='-') {
+                continue;
+            }
+
+            int carry = 0;
+            int n1Digit = s1.charAt(i)-'0';
+            i2=0;
+
+
+
+            for(int j=n2-1;j>=0;j--){
+                if (s2.charAt(j)=='-') {
+                    continue;
+                }
+                int n2Digit=  s2.charAt(j)-'0';
+                int sum = 
+            }
+        }
+
     } 
     public static void main(String[] args) {
         
