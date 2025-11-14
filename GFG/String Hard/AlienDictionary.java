6ebd0581ad2035a08b2c572afcb11f45ef6771c1
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-14 21:51:29 +0530

    find order

--- GFG/String Hard/AlienDictionary.java
+++ GFG/String Hard/AlienDictionary.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class AlienDictionary {
+
+    static String findOrder(String[] words) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int inDegreee[] = new int[26];
+
+        boolean exists[] = new boolean[26];
+        for (int i = 0; i < 26; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (String word : words) {
+            for (char ch : word.toCharArray()) {
+                exists[ch - 'a'] = true;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
