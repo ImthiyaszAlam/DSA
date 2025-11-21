75b13c903610afaaabf072f351e55e6b311c1c1e
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-21 22:49:29 +0530

    Two string anagram

---
+++ GFG/String Hard/TwoStringAnagram.java
@@ -0,0 +1,19 @@
+public class TwoStringAnagram {
+
+    static int remAnagram(String s1, String s2) {
+        int count1[] = new int[26];
+        int count2[] = new int[26];
+
+        for (char ch : s1.toCharArray()) {
+            count1[ch - 'a']++;
+        }
+
+        for (char ch : s2.toCharArray()) {
+            count2[ch - 'a']++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
