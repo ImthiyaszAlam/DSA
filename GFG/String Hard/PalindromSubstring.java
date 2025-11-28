2cc460c82db5a25c12a433015f791f43c6627a60
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-11-28 22:56:34 +0530

    Palindrome Substring

---
+++ GFG/String Hard/PalindromSubstring.java
@@ -0,0 +1,17 @@
+public class PalindromSubstring {
+
+
+    static boolean isPalindrome(String str,int start ,int end ){
+        while (start<end) {
+            if (str.charAt(start)!=str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
