ad7031ee915378cf902fc0a6e925c121d207ccb6
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-01 23:52:21 +0530

    Palindrome Substring

--- GFG/String Hard/PalindromSubstring.java
+++ GFG/String Hard/PalindromSubstring.java
@@ -2,10 +2,9 @@ import java.util.ArrayList;
 
 public class PalindromSubstring {
 
-
-    static boolean isPalindrome(String str,int start ,int end ){
-        while (start<end) {
-            if (str.charAt(start)!=str.charAt(end)) {
+    static boolean isPalindrome(String str, int start, int end) {
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
                 return false;
             }
             start++;
@@ -14,21 +13,35 @@ public class PalindromSubstring {
         return true;
     }
 
+    static ArrayList<Integer> palQueries(String s, int[][] queries) {
 
-    static ArrayList<Integer> palQueries(String s,int [][] queries){
+        ArrayList<Integer> result = new ArrayList<>();
 
-     ArrayList<Integer> result = new ArrayList<>();
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1];
 
-     for(int [] query:queries ){
-        int start = query[0];
-        int end = query[1];
-        
-     }
-        
+            if (isPalindrome(s, start, end)) {
+                result.add(1);
+            } else {
+                result.add(0);
+            }
+        }
+
+        return result;
+
     }
 
-
     public static void main(String[] args) {
-        
+
+        String s = "abaaabaaaba";
+        int queries[][] = {
+                { 0, 10 },
+                { 5, 8 },
+                { 2, 5 },
+                { 5, 9 }
+        };
+
+        palQueries(null, queries);
     }
 }
