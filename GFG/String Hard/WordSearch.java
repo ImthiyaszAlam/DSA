4b3c68f5005ffc53f2baf3eb5feb322b7a68c33c
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-02 23:11:07 +0530

    Valid coord

--- GFG/String Hard/WordSearch.java
+++ GFG/String Hard/WordSearch.java
@@ -1,5 +1,18 @@
 public class WordSearch {
-    public static void main(String[] args) {
+
+    static boolean validCoord(int x, int y, int m, int n) {
+        if (x >= 0 && x < m && y >= 0 && x < n) {
+            return true;
+        }
+        return false;
+    }
+
+
+    static boolean findWord(){
         
+    }
+
+    public static void main(String[] args) {
+
     }
 }
