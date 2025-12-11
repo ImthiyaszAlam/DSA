f2f88dcff4aecbe99f19cc85b1802504a0a1c3a2
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-11 22:07:52 +0530

    findMatch

--- GFG/String Hard/WordSearch4.java
+++ GFG/String Hard/WordSearch4.java
@@ -1,5 +1,23 @@
 public class WordSearch4 {
+
+    static boolean findMatch(char[][] mat, String word, int x, int y, int widX) {
+
+        int wLen = word.length();
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if (widX == wLen) {
+            return true;
+        }
+
+        if (x < 0 || y < 0 || x >= n || y >= m) {
+            return false;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
