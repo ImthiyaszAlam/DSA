36c0fb1a90cc8f53b46c91f6abc803dec10352f1
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-18 22:20:43 +0530

    Print Alternate Result

--- GFG/Array Basics/Alternate.java
+++ GFG/Array Basics/Alternate.java
@@ -4,6 +4,8 @@ public class Alternate {
 
     public static void main(String[] args) {
 
+        int [] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(returnAlternate(arr));
     }
 
     static ArrayList<Integer> returnAlternate(int[] arr) {
