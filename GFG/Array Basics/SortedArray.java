e64cd2fc1fa1e384c6015fbf8b39f0bd56eb6cd3
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-23 21:28:13 +0530

    Is sorted

---
+++ GFG/Array Basics/SortedArray.java
@@ -0,0 +1,21 @@
+public class SortedArray {
+
+    static boolean isSortedHelper(int arr[], int n) {
+        if (n == 0 || n == 1) {
+            return true;
+        }
+        return arr[n - 1] >= arr[n - 2] && isSortedHelper(arr, n - 1);
+
+    }
+
+    static boolean isSorted(int arr[], int length) {
+        return isSortedHelper(arr, length);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        int n = arr.length;
+        System.out.println(isSorted(arr, n));
+        ;
+    }
+}
