6bf825dc12e415c1fce1b821122c19b4f16dc97b
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-25 22:42:56 +0530

    Remove from sorted array logic

--- GFG/Array Basics/RemoveDuplicates.java
+++ GFG/Array Basics/RemoveDuplicates.java
@@ -17,5 +17,10 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
+        int arr[] = { 1, 2, 2, 3, 4, 4, 4, 5, 5, 6 };
+        int size = removeDuplicates(arr);
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i] + " ");
+        }
     }
 }
