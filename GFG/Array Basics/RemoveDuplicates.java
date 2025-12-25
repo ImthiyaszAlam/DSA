4cad3f38b5a2544475bd076e5f5b6505db7a80a4
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-25 22:12:53 +0530

    Remove from sorted array

--- GFG/Array Basics/RemoveDuplicates.java
+++ GFG/Array Basics/RemoveDuplicates.java
@@ -2,18 +2,20 @@ import java.util.HashSet;
 
 public class RemoveDuplicates {
 
-
-    static int removeDuplicates(int arr[]){
+    static int removeDuplicates(int arr[]) {
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i=0;i<arr.length;i++){
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
             if (!set.contains(arr[i])) {
                 set.add(arr[i]);
-                
+                arr[idx++] = arr[i];
             }
         }
+        return idx;
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
