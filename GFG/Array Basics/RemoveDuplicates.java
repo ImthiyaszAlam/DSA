7b2619fe3b0f348a8bc9c0a235d6942058fd504c
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-12-28 22:50:59 +0530

    Duplicates

--- GFG/Array Basics/RemoveDuplicates.java
+++ GFG/Array Basics/RemoveDuplicates.java
@@ -17,7 +17,7 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-        int arr[] = { 1, 2, 2, 3, 4, 4, 4, 5, 5, 6 };
+        int arr[] = { 1, 2, 2, 3, 4, 4, 4, 5, 5, 6,7 };
         int size = removeDuplicates(arr);
         for (int i = 0; i < size; i++) {
             System.out.println(arr[i] + " ");
