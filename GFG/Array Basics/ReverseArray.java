c899269808e1f002cbef261349e36aa6f45dd170
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2026-01-02 21:50:09 +0530

    ReverseArray using Two Pointer

--- GFG/Array Basics/ReverseArray.java
+++ GFG/Array Basics/ReverseArray.java
@@ -1,5 +1,21 @@
 public class ReverseArray {
 
+    public static void reverseArrayTwoPointer(int[] arr){
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+
+
+        while (left<right) {
+            int temp =arr[left];
+            arr[left] = arr[right];
+            arr[right]=temp;
+            
+        left++;
+        right--;
+        }
+    }
+
     public static void reverseArray(int arr[]) {
         int n = arr.length;
         int tempArr[] = new int[n];
@@ -14,7 +30,7 @@ public class ReverseArray {
 
     public static void main(String[] args) {
         int arr[] = { 1, 13, 4, 2, 11, 6 };
-        reverseArray(arr);
+        reverseArrayTwoPointer(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ",");
         }
