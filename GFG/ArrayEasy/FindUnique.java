cf83c784956b2b69dd317a7a6fbf52b6fa0be9e3
alam <developerimthiyas+imthiyas@gmail.com>
2026-02-04 09:34:16 +0530

    Hashmap implementation

--- GFG/ArrayEasy/FindUnique.java
+++ GFG/ArrayEasy/FindUnique.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindUnique {
 
     public static int findUnique(int[] arr) {
-        int count = 0;
+
+
+        Map<Integer,Integer> freq = new HashMap<>();
+                int count = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    arr[j] = -1;
-                    count++;
+                freq.put(i, freq.getOrDefault(freq, 1));
+                
                 }
             }
         }
