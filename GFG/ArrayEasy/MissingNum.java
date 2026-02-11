a3a2a2d565907775c4467c888f33cf873ce17fef
alam <developerimthiyas+imthiyas@gmail.com>
2026-02-11 09:16:33 +0530

    Missing Num

--- GFG/ArrayEasy/MissingNum.java
+++ GFG/ArrayEasy/MissingNum.java
@@ -1,5 +1,18 @@
 public class MissingNum {
+
+    public static int missingNum(int[] nums) {
+        int n = nums.length+1;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        return expectedSum - actualSum;
+    }
+
     public static void main(String[] args) {
-        
+        int[] nums = { 1, 2, 3, 5 };
+      
+        System.out.println("The missing number is :" + missingNum(nums));
     }
 }
