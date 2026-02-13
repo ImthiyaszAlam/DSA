1858ca36a388ef75a4e0e6fe6b00ceb67180e5e6
alam <developerimthiyas+imthiyas@gmail.com>
2026-02-13 08:58:58 +0530

    Find Duplicate

--- GFG/ArrayEasy/Repeating1toNm1.java
+++ GFG/ArrayEasy/Repeating1toNm1.java
@@ -1,13 +1,28 @@
 public class Repeating1toNm1 {
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,3};
-        int n = arr.length-1;
-        int actualSum = 0;
-        int expectedSum = n*(n+1)/2;
 
-        for(int i:arr){
-            actualSum+=i;
+    public static int findRepeating(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return arr[i];
+                }
+            }
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 3 };
+        int n = arr.length - 1;
+        int actualSum = 0;
+        int expectedSum = n * (n + 1) / 2;
+
+        for (int i : arr) {
+            actualSum += i;
+        }
+
+        System.out.println("Repeating number is:" + findRepeating(arr));
 
     }
 }
